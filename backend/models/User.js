eec115e9c21ad5ee35d639b2b03ce7ab309981a5
jvishwa06 <jvishu06@gmail.com>
2025-03-19 22:55:33 +0530

    Removed Created and Updated fields in User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -32,14 +32,6 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    created_at: {
-      type: Date,
-      default: Date.now, 
-    },
-    updated_at: {
-      type: Date,
-      default: Date.now, 
-    },
   },
   {
     timestamps: true, 
