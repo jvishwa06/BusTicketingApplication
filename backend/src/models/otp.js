447796c6132a8b4c40f52d4f2e9f6d6ca7fdd822
jvishwa06 <jvishu06@gmail.com>
2025-03-25 18:32:12 +0530

    resolved errors in test

--- backend/src/models/otp.js
+++ backend/src/models/otp.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const otpSchema = new mongoose.Schema({
-  userId: { type: string, required: true }, 
+  userId: { type: mongoose.Schema.Types.ObjectId, required: true }, 
   otp: { type: Number, required: true },
   expiresAt: { type: Date, required: true },
 });
