bb72c1234f2fa95fdd399f456f9868a42994cb67
“jvishwa06” <jvishu06@gmail.com>
2025-04-02 23:23:26 +0530

    trip id routes modified

--- backend/src/routes/tripRoutes.js
+++ backend/src/routes/tripRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post('/', authMiddleware.authenticate, authMiddleware.authorizeRoles('admin', 'operator'), TripController.createTrip);
 router.get('/', authMiddleware.authenticate, authMiddleware.authorizeRoles('admin', 'operator'),TripController.getAllTrips);
 router.get('/search', authMiddleware.authenticate, TripController.getFilteredTrips);
-router.put('/:tripId', authMiddleware.authenticate, TripController.updateTrip);
-router.delete('/:tripId', authMiddleware.authenticate, TripController.deleteTrip);
+router.put('/:tripId', authMiddleware.authenticate, authMiddleware.authorizeRoles('admin', 'operator'), TripController.updateTrip);
+router.delete('/:tripId', authMiddleware.authenticate, authMiddleware.authorizeRoles('admin', 'operator'), TripController.deleteTrip);
 
 export default router;
