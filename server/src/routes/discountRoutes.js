9b98b4c6ad8c60697533007771b52015d5dbd7eb
“jvishwa06” <jvishu06@gmail.com>
2025-04-15 18:19:26 +0530

    rate limiter configuration

--- server/src/routes/discountRoutes.js
+++ server/src/routes/discountRoutes.js
@@ -5,9 +5,9 @@ import authMiddleware from '../middlewares/authMiddleware.js';
 const router = express.Router();
 router.use(authMiddleware.authenticate);
 
-router.post('/validate', authMiddleware.authorizeRoles('user'), DiscountController.validateDiscount);
+// router.post('/validate', authMiddleware.authorizeRoles('user'), DiscountController.validateDiscount);
 router.post('/', authMiddleware.authorizeRoles('operator'),  DiscountController.createDiscount);
-router.get('/', authMiddleware.authorizeRoles('operator'),  DiscountController.getAllDiscounts);
+router.get('/', authMiddleware.authorizeRoles('operator'), DiscountController.getAllDiscounts);
 router.get('/:code', authMiddleware.authorizeRoles('operator'),  DiscountController.getDiscountByCode);
 router.put('/:id', authMiddleware.authorizeRoles('operator'),  DiscountController.updateDiscount);
 router.delete('/:id', authMiddleware.authorizeRoles('operator'),  DiscountController.deleteDiscount);
--- server/src/server.js
+++ server/src/server.js
@@ -36,9 +36,9 @@ app.use('/buses', rateLimiter.apiRateLimiter, busRoutes);
 app.use('/trips', rateLimiter.apiRateLimiter, tripRoutes);
 app.use('/bookings', rateLimiter.bookingRateLimiter, bookingRoutes);
 app.use('/payments', rateLimiter.paymentRateLimiter, paymentRoutes);
-app.use('/admin', rateLimiter.apiRateLimiter, adminRoutes);
+app.use('/admin', adminRoutes);
 app.use('/ratings', rateLimiter.apiRateLimiter, ratingRoutes);
-app.use('/analytics', rateLimiter.apiRateLimiter, analyticsRoutes); 
+app.use('/analytics', analyticsRoutes); 
 app.use('/discounts', rateLimiter.apiRateLimiter, discountRoutes); 
 
 const PORT = process.env.PORT || 5001;
