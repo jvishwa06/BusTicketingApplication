6f7353af22676211e7b2b537d21fa80c5dffa636
“jvishwa06” <jvishu06@gmail.com>
2025-04-18 23:16:13 +0530

    removed discount routes

--- server/src/server.js
+++ server/src/server.js
@@ -10,7 +10,6 @@ import bookingRoutes from './routes/bookingRoutes.js';
 import paymentRoutes from './routes/paymentRoutes.js';
 import adminRoutes from './routes/adminRoutes.js';
 import ratingRoutes from './routes/ratingRoutes.js';
-import discountRoutes from './routes/discountRoutes.js'; 
 import { requestLogger } from './utils/logger.js';
 import rateLimiter from './middlewares/rateLimiterMiddleware.js';
 
@@ -37,7 +36,6 @@ app.use('/trips', rateLimiter.apiRateLimiter, tripRoutes);
 app.use('/bookings', rateLimiter.bookingRateLimiter, bookingRoutes);
 app.use('/payments', rateLimiter.paymentRateLimiter, paymentRoutes);
 app.use('/ratings', rateLimiter.apiRateLimiter, ratingRoutes);
-app.use('/discounts', rateLimiter.apiRateLimiter, discountRoutes); 
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
